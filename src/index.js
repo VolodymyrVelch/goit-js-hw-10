92050bb50e921974fd3d05c90cf2dd5538299e23
Volodymyr Velch <velch83@gmail.com>
2022-08-18 18:53:59 +0300

    js-hw-10-commit2

--- src/index.js
+++ src/index.js
@@ -13,10 +13,19 @@ refs = {
   input: document.querySelector('input'),
 };
 
-// refs.input.addEventListener('input', debounce(onInputchange, DEBOUNCE_DELAY));
-refs.input.addEventListener('change', onInputchange);
+refs.input.addEventListener('input', debounce(onInputchange, DEBOUNCE_DELAY));
 
 function onInputchange(e) {
-  const inputData = e.currentTarget.value.trim();
-  fetchCountries(inputData).then(country => console.log(country[2]));
+  const inputData = refs.input.value.trim();
+  fetchCountries(inputData).then(country => {
+    console.log(country);
+    renderCountry(country);
+  });
+}
+
+function renderCountry({ name, capital, population, flags, languages }) {
+  console.log(name, capital, population, flags, languages);
+  // console.log(
+  //   `name ${name.official} capital ${capital} population ${population} flags ${flags.svg}`
+  // );
 }
