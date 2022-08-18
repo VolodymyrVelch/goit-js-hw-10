e0bf799fcb931ff2dff08a7846189ce8a5f3ac5a
Volodymyr Velch <velch83@gmail.com>
2022-08-18 23:17:26 +0300

    js-hw-10-markup-added

--- src/index.js
+++ src/index.js
@@ -2,30 +2,36 @@ import './css/styles.css';
 
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
+import { renderCountry } from './js/marcup-country';
 
 // import countryCard from './templates/country-card.hbs';
 // import cards from './templates/country-card.hbs';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 
 refs = {
   input: document.querySelector('input'),
+  markup: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener('input', debounce(onInputchange, DEBOUNCE_DELAY));
 
 function onInputchange(e) {
+  e.preventDefault;
   const inputData = refs.input.value.trim();
-  fetchCountries(inputData).then(country => {
-    console.log(country);
-    renderCountry(country);
-  });
-}
-
-function renderCountry({ name, capital, population, flags, languages }) {
-  console.log(name, capital, population, flags, languages);
-  // console.log(
-  //   `name ${name.official} capital ${capital} population ${population} flags ${flags.svg}`
-  // );
+  fetchCountries(inputData)
+    // .then(country => {
+    //   console.log(country);
+    // })
+    .then(countries => {
+      countries.map(country => {
+        const markup = renderCountry(country);
+        refs.markup.innerHTML = markup;
+      });
+    })
+    .catch(
+      error => console.log(error)
+      // Notiflix.Notify.warning('Oops, there is no country with that name')
+    );
 }
---
+++ src/js/marcup-country.js
@@ -0,0 +1,22 @@
+export function renderCountry({ name, capital, population, flags, languages }) {
+  console.log(name.official, capital, population, flags, languages);
+  let langList = Object.values(languages);
+  //   //   let lang = langList.split(' ');
+  //   console.log(langList.splite(''));
+
+  return `  <div class="country-block">
+    <img src="${flags.svg}" alt="flag" height='30' wtdth='30'>
+    <h2 class="country-info_name">${name.official}</h2>
+  </div>
+  <ul>
+    <li class="country-info_item">
+      <p class="country-info_text">Capital: <span class="country-info_description"> ${capital} </span> </p>
+    </li>
+    <li class="country-info_item">
+      <p class="country-info_text">Population: <span class="country-info_description"> ${population} </span> </p>
+    </li>
+    <li class="country-info_item">
+      <p class="country-info_text">Languages: <span class="country-info_description"> ${langList} </span> </p>
+    </li>
+  </ul>`;
+}
