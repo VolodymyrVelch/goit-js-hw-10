f47d515be31707eec067c2d28a654abf8008b2ae
Volodymyr Velch <velch83@gmail.com>
2022-08-20 19:36:01 +0300

    some correction

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
-const mainUrl = 'https://restcountries.com/v3.1/name/';
-const fiilterCountryData = 'fields=name,capital,population,flags,languages';
-
 export function fetchCountries(name) {
+  const mainUrl = 'https://restcountries.com/v3.1/name/';
+  const fiilterCountryData = 'fields=name,capital,population,flags,languages';
   return fetch(`${mainUrl}${name}?${fiilterCountryData}`).then(response =>
     response.json()
   );
