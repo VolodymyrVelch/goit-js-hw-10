a5c80c269fb0f37b5592de17428677815f4f8ba5
Volodymyr V <Volodymyrv@Veronikas-MacBook-2.local>
2022-08-21 17:53:34 +0300

    add-one-more-cheacking

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,10 @@
 export function fetchCountries(name) {
   const mainUrl = 'https://restcountries.com/v3.1/name/';
   const fiilterCountryData = 'fields=name,capital,population,flags,languages';
-  return fetch(`${mainUrl}${name}?${fiilterCountryData}`).then(response =>
-    response.json()
-  );
+  return fetch(`${mainUrl}${name}?${fiilterCountryData}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
